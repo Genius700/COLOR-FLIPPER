5ba7584677f6e2c8952dca639aab213156d9d0bb
Genius700 <101404294+Genius700@users.noreply.github.com>
2023-06-08 00:56:21 +0100

    flipper

---
+++ app.js
@@ -0,0 +1,11 @@
+let colors = ['red', "blue", "rgba(133,122,200)", "#f15025"]
+const button = document.getElementById('bt')
+const colour = document.querySelector(".colour")
+
+bt.addEventListener("click", function(){
+
+    document.body.style.backgroundColor=[colors]
+    colour.style.backgroundColor= [colors]
+    colour.textContent=[colors]
+
+});
