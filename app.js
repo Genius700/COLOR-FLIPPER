d1f176966dd6ecf919519bb0cf17de5c6fa717a2
Boateng Oduro Kelvin <83387330+codingbagh@users.noreply.github.com>
2023-06-08 01:08:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let colors = ['red', "blue", "rgba(133,122,200)", "#f15025"]
 const button = document.getElementById('bt')
 const colour = document.querySelector(".colour")
-
+let counter = 0;
 bt.addEventListener("click", function(){
-
-    document.body.style.backgroundColor=[colors]
-    colour.style.backgroundColor= [colors]
-    colour.textContent=[colors]
-
+if(counter<colors.length){
+    document.body.style.backgroundColor=colors[counter]
+    colour.style.backgroundColor= colors[counter]
+    colour.textContent=colors[counter]
+}
 });
